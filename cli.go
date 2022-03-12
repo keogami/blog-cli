434cb40078c8adafb7f27d0c78bdbfdda7e94123
keogami <41939011+keogami@users.noreply.github.com>
2022-03-12 12:34:23 +0530

    made a basic app

---
+++ cli.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+  "github.com/urfave/cli/v2"
+)
+
+func MakeApp() *cli.App {
+  return cli.NewApp()
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "os"
+
+func main() {
+  app := MakeApp()
+
+  app.Run(os.Args)
+}
