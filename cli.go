f8ae2849052b26ca5773ec5ed4e096eb0fb3e835
keogami <41939011+keogami@users.noreply.github.com>
2022-03-12 17:31:00 +0530

    created dummy sub commands

--- cli.go
+++ cli.go
@@ -5,5 +5,39 @@ import (
 )
 
 func MakeApp() *cli.App {
-  return cli.NewApp()
+  return &cli.App{
+    Name: "blog",
+    Usage: "a cli tool to manage blog entries",
+    Description: "a cli tool to post/put/delete blog entries by interfacing with the api running at $BLOG_API_URL",
+    Commands: []*cli.Command{
+      {
+        Name: "init",
+        Aliases: []string{ "i" },
+        Description: "initializes the current directory as a blog post entry",
+        Usage: "initializes the current directory",
+        Action: Init,
+      },
+      {
+        Name: "post",
+        Aliases: []string{ "po" },
+        Description: "posts the contents of the current blog directory to $BLOG_API_URL",
+        Usage: "posts a blog entry",
+        Action: Post,
+      },
+      {
+        Name: "put",
+        Aliases: []string{ "pu" },
+        Description: "puts the contents of the current blog directory to $BLOG_API_URL/:slug",
+        Usage: "puts a blog entry",
+        Action: Put,
+      },
+      {
+        Name: "delete",
+        Aliases: []string{ "d" },
+        Description: "deletes the contents of the current blog directory from $BLOG_API_URL/:slug",
+        Usage: "deletes a blog entry",
+        Action: Delete,
+      },
+    },
+  }
 }
