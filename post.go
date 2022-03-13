dcd3b6420a3bc98c2f0175e7d2f9e7b167fd38f1
keogami <41939011+keogami@users.noreply.github.com>
2022-03-14 00:43:46 +0530

    improved the post sub-command

--- post.go
+++ post.go
@@ -1,19 +1,36 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io"
+	"strings"
 
 	"github.com/urfave/cli/v2"
 )
 
 func Post(c *cli.Context) error {
-  p, err := NewCreateParams()
+  params, err := NewCreateParams()
   if err != nil {
     return cli.Exit(fmt.Errorf("Couldn't load post parameters: %w", err), 2)
   }
 
-  d, _ := json.MarshalIndent(p, "", "  ")
-  fmt.Println(string(d))
+  p, err := APIPath("/blog")
+  if err != nil {
+    return cli.Exit(fmt.Errorf("Couldn't construct API route: %w", err), 4)
+  }
+
+  res, err := PostJson(p, params)
+  if err != nil {
+    return cli.Exit(fmt.Errorf("Couldn't send post parameters: %w", err), 3)
+  }
+  defer res.Body.Close()
+
+  status := res.StatusCode
+  b := new(strings.Builder)
+
+  io.Copy(b, res.Body)
+
+  fmt.Println(status)
+  fmt.Println(b.String())
   return nil
 }
